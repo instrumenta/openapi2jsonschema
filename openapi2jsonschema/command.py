8c2fdc48898cf2d0d5927a2a7858dfa9732be0c1
Will Thames <wthames@skedulo.com>
2018-08-23 14:48:57 +1000

    Generate JSONSchemaProps and CustomResourceDefinitions
    JSONSchemaProps and resources that rely on JSONSchemaProps such as
    CustomResourceDefinitions are only problematic in standalone mode
    because they generate circular references.
    
    Generate the schema files for such resources when not in standalone
    mode

--- openapi2jsonschema/command.py
+++ openapi2jsonschema/command.py
@@ -166,7 +166,7 @@ def default(output, schema, prefix, stand_alone, kubernetes, strict):
 
             # This list of Kubernets types carry around jsonschema for Kubernetes and don't
             # currently work with openapi2jsonschema
-            if kubernetes and kind in ["jsonschemaprops", "jsonschemapropsorarray", "customresourcevalidation", "customresourcedefinition", "customresourcedefinitionspec", "customresourcedefinitionlist", "customresourcedefinitionspec", "jsonschemapropsorstringarray", "jsonschemapropsorbool"]:
+            if kubernetes and stand_alone and kind in ["jsonschemaprops", "jsonschemapropsorarray", "customresourcevalidation", "customresourcedefinition", "customresourcedefinitionspec", "customresourcedefinitionlist", "customresourcedefinitionspec", "jsonschemapropsorstringarray", "jsonschemapropsorbool"]:
                 raise UnsupportedError("%s not currently supported" % kind)
 
             if stand_alone:
